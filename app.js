f9a4c70facb94a322773744b36e713249b359088
Talal Alrwas <85613292+talal12bfk@users.noreply.github.com>
2023-02-26 11:42:23 +0300

    init-1

---
+++ app.js
@@ -0,0 +1,26 @@
+let body = document.body;
+let html = document.getElementsByTagName("html")[0];
+
+let menuBtn = document.getElementById('menuBtn');
+menuBtn.addEventListener("click", () => {
+  console.log("hello");
+});
+
+let langBtn = document.getElementById('langBtn');
+langBtn.addEventListener("click", () => {
+  if (html.lang == 'ar') {
+    html.setAttribute('lang', 'en')
+    langBtn.innerText = 'arabic'
+    setLang('en');
+  } else {
+    html.setAttribute('lang', 'ar')
+    langBtn.innerText = 'english'
+    setLang('ar');
+  }
+})
+
+function setLang(lang) {
+  
+}
+
+console.log(langBtn.innerText)
